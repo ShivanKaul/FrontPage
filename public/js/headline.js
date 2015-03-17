a56adf353ef45b09933a4c0664871a3fa6ed6cb0
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2015-03-17 09:30:58 -0400

    Trying to filter "Letters to the editor"

--- public/js/headline.js
+++ public/js/headline.js
@@ -15,10 +15,13 @@ function call_api() {
 				var results = queryResult.results;
 				var numResults = queryResult.num_results;
 				if (numResults > 0) {
-					var seed = Math.min(19, numResults);
-					var randomNum = Math.floor((Math.random()*seed));
-					console.log(randomNum);
-					var title = results[randomNum].title;
+					var title = "Letters to the Editor";
+					while (title == "Letters to the Editor") {
+						var seed = Math.min(19, numResults);
+						var randomNum = Math.floor((Math.random()*seed));
+						console.log(randomNum);
+						title = results[randomNum].title;
+					}
 					var link = results[randomNum].url;
 					var abstract = results[randomNum].abstract;
 					document.getElementById("insert").setAttribute('href', link);
