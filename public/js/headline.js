c81683d0b1267d41194d71cd80d3f9ddf6eeb5d7
Shivan Kaul Sahib <shivankaul.1993@gmail.com>
2015-05-19 01:39:24 -0400

    Update headline.js

--- public/js/headline.js
+++ public/js/headline.js
@@ -1,39 +1,53 @@
 function call_api() {
-	var queryURL = "https://api.nytimes.com/svc/news/v3/content/all/science/.jsonp?api-key=6b0b5eb0ff211fe59f2978cee9d8a906:14:71584868";
+	var public = secret
+	var queryURL = "https://api.nytimes.com/svc/news/v3/content/all/science/.json?api-key=" + public.API_KEY
 	$(document).ready(function(){
 		$.ajax({
 			url: queryURL,
-			dataType: "jsonp",
-			crossOrigin: true,
+			dataType: "json",
 			statusCode: {
 	        	502: function () {
 	        		console.log("Error 502 thrown.")
 	        	}
 	        },
 			success: function (queryResult) {
-				// get array of all software projects
+				// get array of all headlines
 				var results = queryResult.results;
 				var numResults = queryResult.num_results;
 				if (numResults > 0) {
-					var title = "Letters to the Editor";
-					while (title == "Letters to the Editor") {
-						var bound = Math.min(19, numResults);
-						var randomNum = Math.floor((Math.random()*bound));
-						console.log(randomNum);
-						title = results[randomNum].title;
-					}
+					var result = getTitle(numResults, results)
+					var title = result[0]
+					var randomNum = result[1]
 					var link = results[randomNum].url;
-					var abstract = results[randomNum].abstract;
+					var abstract = "&ldquo;" + results[randomNum].abstract + "&rdquo;";
 					document.getElementById("insert").setAttribute('href', link);
+					document.getElementById("insert").setAttribute('title', "Link to NYT article");
 					document.getElementById("insert").innerHTML = title;
 					document.getElementById("abstract").innerHTML = abstract;
-					// document.write(title);
 				}
 				else {
 					document.write("No news found!");
 				}
-			}
+			},
+			error: function(statusCode, errorThrown) {
+        			if (statusCode.status == 0) {
+            				document.getElementById("abstract").innerHTML = "You're offline, unfortunately. FrontPage is powered by The Internet &#8482;";
+        			}
+    			}
 		});
 	});
 }
+
+function getTitle(numResults, results) {
+	var bound = Math.min(19, numResults);
+	var randomNum = Math.floor((Math.random()*bound));
+	var title = results[randomNum].title;
+	while (title == "Letters to the Editor" || title == "Reactions") {
+		var newResult = getTitle(numResults, results)
+		title = newResult[0];
+		randomNum = newResult[1]
+	}
+	return [title, randomNum]
+}
+
 call_api();
